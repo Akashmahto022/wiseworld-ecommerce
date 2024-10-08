7fbc92688221d2e7aebc82fc37544d473f1b6284
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-08 12:28:16 +0530

    connected a mongoDB with the server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-
-
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 
 // create a database connection
-mongoose.connect('mongodb+srv://akashmahto2272003:wiseworld5060@cluster0.0xrfe.mongodb.net/')
-.then(()=>console.log('MongoDB Connected'))
-.catch(()=>console.log('error connecting in MongoDB'))
+mongoose
+  .connect(
+    "mongodb+srv://akashmahto2272003:wiseworld5060@cluster0.0xrfe.mongodb.net/"
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(() => console.log("error connecting in MongoDB", Error));
 
-const app = express()
-const PORT = process.env.PORT || 4000
-
+const app = express(); 
+const PORT = process.env.PORT || 4000;
 
 app.use(
-    cors({
-        
-    })
-)
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "DELETE", "PUT"],  
+    Credential: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => console.log(`server is running on the port http://localhost:${PORT}`));
