95f7085279ca3603ec7df5ced7c7b5e09ddd6154
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-21 22:25:54 +0530

    connected mongoDB

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
-    console.log("mongoDB connected successfully", connectionInstance);
+    console.log("mongoDB connected successfully");
   } catch (error) {
     console.log("error while connecting mongoDB", error.message);
     process.exit(1);
--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ dotenv.config({
 connectDB()
 .then(()=>{
   app.listen(process.env.PORT || 5000, ()=>{
-    console.log(`server is running at http://localhost${Process.env.PORT}`)
+    console.log(`server is running at http://localhost:${process.env.PORT}`)
   })
 })
 .catch((error)=>{
