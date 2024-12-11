52c2fd8b03217e0db2f9dd09c3f53641f1327289
Akashmahto022 <akashmahto1169@gmail.com>
2024-12-11 12:37:12 +0530

    create config for add product

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -40,3 +40,71 @@ export const loginFormControllers = [
     },
 
 ]
+
+export const addProductFromElements = [
+    {
+        label: "Title",
+        name: "title",
+        componentType: "input",
+        type: "text",
+        placeholder: "Enter product title"
+    },
+    {
+        label: "Description",
+        name: "description",
+        componentType: "textarea",
+        placeholder: "Enter product description"
+    },
+    {
+        label: "Category",
+        name: "category",
+        componentType: "select",
+        options:[
+            {id: "men", label: "Men"},
+            {id: "women", label: "Women"},
+            {id: "kids", label: "Kids"},
+            {id: "accessories", label: "Accessories"},
+            {id: "footwear", label: "Footwear"},
+            {id: "electronic", label: "Electronic"},
+            {id: "ferniture", label: "Ferniture"},
+        ]
+    },
+    {
+        label: "Brand",
+        name: "brand",
+        componentType: "select",
+        options:[
+            {id: "nike", label: "Nike"},
+            {id: "adidas", label: "Adidas"},
+            {id: "puma", label: "Puma"},
+            {id: "levi", label: "Levi's"},
+            {id: "zara", label: "Zara"},
+            {id: "h&m", label: "H&M"},
+            {id: "gucci", label: "Gucci"},
+            {id: "dior", label: "Dior "},
+            {id: "fabindia", label: "Fabindia "},
+        ]
+
+    },
+    {
+        label: "Price",
+        name: "price",
+        componentType: "input",
+        type: "number",
+        placeholder: "Enter product price"
+    },
+    {
+        label: "Sale Price",
+        name: "saleprice",
+        componentType: "input",
+        type: "number",
+        placeholder: "Enter product price"
+    },
+    {
+        label: "Total Stock",
+        name: "totalStock",
+        componentType: "input",
+        type: "number",
+        placeholder: "Enter total stock"
+    }
+]
